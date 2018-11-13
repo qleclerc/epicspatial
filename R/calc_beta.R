95aeb13200dc2c1af938203b60f05c334116fa42
Quentin Leclerc <39052287+qleclerc@users.noreply.github.com>
2018-11-13 12:32:14 +0000

    Update for speed
    Increased speed for beta calculation by only using the contact matrix to derive the NGM (possible here since we are assuming homogeneous age distribution)

--- R/calc_beta.R
+++ R/calc_beta.R
@@ -32,6 +32,8 @@
 
 calc_beta = function(spatial_data, dist_kernel, contact_mat, R0=1.8, sigma=1/2.6){
 
+  #first time to properly calculate expanded_D:
+
   if(class(spatial_data) == "RasterLayer"){
 
     populated_areas = which(!is.na(spatial_data@data@values))
@@ -102,11 +104,6 @@ calc_beta = function(spatial_data, dist_kernel, contact_mat, R0=1.8, sigma=1/2.6
   kxeye = matrix(1, nrow=num_areas, ncol=num_areas) - keye
   Cbar = kronecker(contact_mat, keye) + kronecker(contact_mat, kxeye)
 
-  DD=(Sstart*K1*Mjover)%*%(t(Kbar)*Cbar)
-
-  X=DD/sigma
-
-
   ### this extra bit just calculates D, necessary for FOI calculation, it's then faster to run the simulation
   Ni=matrix(NN0,nrow=length(NN0),ncol=length(NN0),byrow=F)
   Nj=t(Ni)
@@ -119,11 +116,96 @@ calc_beta = function(spatial_data, dist_kernel, contact_mat, R0=1.8, sigma=1/2.6
   ###
 
 
+
+
+  #second time to calculate beta fast:
+
+  dist_kernel = matrix(1,1,1)
+
+  if(class(spatial_data) == "RasterLayer"){
+
+    populated_areas = which(!is.na(spatial_data@data@values))
+
+    N = spatial_data@data@values[populated_areas]
+
+    N = sum(N)
+
+  } else if(class(spatial_data) == "SpatialPolygonsDataFrame"){
+
+    N = spatial_data@data$population
+
+    N = sum(N)
+
+  } else{
+
+    stop("Incorrect spatial dataset! The dataset must be either a RasterLayer or a SpatialPolygonsDataFrame.")
+
+  }
+
+
+  num_areas = dim(dist_kernel)[1]   #derive number of areas from distance kernel
+  num_ages = dim(contact_mat)[1]    #derive number of age categories from contact matrix
+
+
+  #!!! work in progress, only supports 4 age categories or no age categories at all right now !!!#
+
+  if(num_ages == 4){
+
+    N = matrix(N, nrow=num_areas, ncol=num_ages)
+
+    #set minimum population in an area to 1:
+    N[which(N<1)] = 1
+
+    NN0 = as.vector(N)
+
+    N[,1] = N[,1]*(5/81)
+    N[,2] = N[,2]*(14/81)
+    N[,3] = N[,3]*(46/81)
+    N[,4] = N[,4]*(16/81)
+    #this way, i in the matrix is the area and j the age group e.g. N[1,1] gives pop 0-4 in area 1
+
+
+  } else if(num_ages == 1){
+
+    N[which(N<1)] = 1
+
+    NN0 = as.vector(N)
+
+  } else {
+
+    stop("Unsupported number of age categories, currently only supports 4 or none.")
+
+  }
+
+
+  Sstart = matrix(N, ncol=1)
+  Sstart = matrix(Sstart, ncol=nrow(Sstart), nrow=nrow(Sstart))
+
+  K1 = kronecker(diag(num_ages), dist_kernel)
+
+  NNbar = matrix(N, ncol=1)
+  Kbar = kronecker(matrix(1,num_ages,num_ages), dist_kernel)
+
+  Mj=t(Kbar)%*%NNbar
+  Mj[which(Mj==0)]=1
+  Mjover=1/Mj
+  Mjover = t(Mjover)
+  Mjover = matrix(rep(Mjover, num_areas*num_ages), nrow=num_areas*num_ages, byrow=T)
+
+  keye = diag(num_areas)
+  kxeye = matrix(1, nrow=num_areas, ncol=num_areas) - keye
+  Cbar = kronecker(contact_mat, keye) + kronecker(contact_mat, kxeye)
+
+  DD=(Sstart*K1*Mjover)%*%(t(Kbar)*Cbar)
+
+  X=DD/sigma
+
   eigen_vals = eigen(X, symmetric=F, only.values = T)$values
 
   R0a = max(Re(eigen_vals))
 
   beta=R0/R0a
+
 
   return(beta)
 
